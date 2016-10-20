800a89640ebf51b0de412796975c4878fdc89301
vatteh <victor@artivest.co>
2016-10-20 00:49:35 -0400

    page 99

--- src/scope.js
+++ src/scope.js
@@ -174,14 +174,51 @@ Scope.prototype.$$postDigest = function(fn) {
 Scope.prototype.$watchGroup = function(watchFns, listenerFn) { 
     var self = this;
     var newValues = new Array(watchFns.length);
-    var oldValues = new Array(watchFns.length); 
-    _.forEach(watchFns, function(watchFn, i) {
-        self.$watch(watchFn, function(newValue, oldValue) { 
+    var oldValues = new Array(watchFns.length);
+    var changeReactionScheduled = false;
+    var firstRun = true;
+
+    if (watchFns.length === 0) { 
+        var shouldCall = true;
+        self.$evalAsync(function() {
+            if (shouldCall) {
+                listenerFn(newValues, newValues, self);
+            }
+        });
+
+        return function() {
+            shouldCall = false; 
+        };
+    }
+
+    function watchGroupListener() {
+        if (firstRun) {
+            firstRun = false;
+            listenerFn(newValues, newValues, self); 
+        } else {
+            listenerFn(newValues, oldValues, self);
+        }
+ 
+        changeReactionScheduled = false; 
+    }
+
+
+    var destroyFunctions = _.map(watchFns, function(watchFn, i) {
+        return self.$watch(watchFn, function(newValue, oldValue) { 
             newValues[i] = newValue;
             oldValues[i] = oldValue;
-            listenerFn(newValues, oldValues, self);
+            if (!changeReactionScheduled) {
+                changeReactionScheduled = true;
+                self.$evalAsync(watchGroupListener);
+            }
         });
     });
+
+    return function() {
+        _.forEach(destroyFunctions, function(destroyFunction) {
+            destroyFunction();
+        });
+    };
 };
 
 module.exports = Scope;
