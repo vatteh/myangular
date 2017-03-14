7bbe0b693a816f8d6fd2b50744a4c2ab03c66094
vatteh <victor@artivest.co>
2017-03-13 23:16:43 -0400

    page 253

--- src/parse.js
+++ src/parse.js
@@ -194,7 +194,7 @@ AST.prototype.primary = function() {
         primary = this.constant();
     }
 
-    if (this.expect('.')) {
+    while (this.expect('.')) {
         primary = {
             type: AST.MemberExpression,
             object: primary,
